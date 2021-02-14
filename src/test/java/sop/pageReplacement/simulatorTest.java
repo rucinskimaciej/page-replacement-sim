6bf415ae9c3f0ad70b70ff30221991ba4503ade6
Rucinski <maciej.rucinski@vimn.com>
2021-02-14 21:17:09 +0100

    ADD GUI

--- src/test/java/sop/pageReplacement/simulatorTest.java
+++ src/test/java/sop/pageReplacement/simulatorTest.java
@@ -7,39 +7,79 @@ import sop.pageReplacement.simulator.lru.LRU;
 import sop.pageReplacement.simulator.optimal.Optimal;
 import sop.pageReplacement.simulator.secondChance.SecondChance;
 
+import javax.swing.*;
+
 class simulatorTest {
 
-    private final static String examInput =
+    @Test
+    void FIFO() {
+        strategyName = "FIFO";
+        input();
+        sim = new FIFO(frameCount, input);
+        sim.execute();
+        promptEnd();
+    }
+
+    @Test
+    void LRU() {
+        strategyName = "LRU";
+        input();
+        sim = new LRU(frameCount, input);
+        sim.execute();
+        promptEnd();
+    }
+
+    @Test
+    void OPTYMALNY() {
+        strategyName = "OPTYMALNY";
+        input();
+        sim = new Optimal(frameCount, input);
+        sim.execute();
+        promptEnd();
+    }
+
+    @Test
+    void DRUGIEJ_SZANSY() {
+        strategyName = "DRUGIEJ SZANSY";
+        input();
+        sim = new SecondChance(frameCount, input);
+        sim.execute();
+        promptEnd();
+    }
 
 
-"                         4)(3)(1)(5)(8)(3)(8)(4)(7)(8)(5)(3)(5)(4)(5)(2)(6)(7)(6)(5                                "
 
 
-                    .replace(")(", " ").replace(",", " ").trim();
-    private final static int[] lectureInput = new int[]{1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5};
+
+
+
+
+
+
+    private void promptEnd() {
+        String format = "Strategia: %s\nIlość ramek: %d\nStrony: %s";
+        JOptionPane.showMessageDialog(null, String.format(format, strategyName, frameCount, input));
+    }
+
+    private void input() {
+        frameCount = getFrames();
+        input = getInput();
+    }
+
+    private String getInput() {
+        String fromDialog = JOptionPane.showInputDialog(String.format("STRATEGIA: %s%nILOŚĆ RAMEK: %d%nWpisz ciąg ramek oddzielonych spacjami, przecinkami lub ')('", strategyName, frameCount));
+        if (fromDialog == null) System.exit(-1);
+        return fromDialog.replace(")(", " ").replace(",", " ").replace(".", " ").trim();
+    }
+
+    private int getFrames() {
+        String strFrameCount = JOptionPane.showInputDialog(String.format("Podaj ilość ramek dla strategii \"%s\":", strategyName));
+        if (strFrameCount == null) System.exit(-1);
+        return Integer.parseInt(strFrameCount);
+    }
+
     private PageReplacementSimulator sim;
-
-    @Test
-    void fifoTest() {
-        sim = new FIFO(3, examInput);
-        sim.execute();
-    }
-
-    @Test
-    void lruTest() {
-        sim = new LRU(3, examInput);
-        sim.execute();
-    }
-
-    @Test
-    void optimalTest() {
-        sim = new Optimal(3, examInput);
-        sim.execute();
-    }
-
-    @Test
-    void secondChanceTest() {
-        sim = new SecondChance(3, examInput);
-        sim.execute();
-    }
+    private String strategyName;
+    private String input;
+    private int frameCount;
 }
